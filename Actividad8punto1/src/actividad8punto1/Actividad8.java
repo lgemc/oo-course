3fbf50437c136fc563ce8330b5f3f114699b22f1
Luis Gerardo Manrique Cardona <lmanrique@truora.com>
2022-10-05 14:59:14 -0500

    Formatear codigo

--- Actividad8punto1/src/actividad8punto1/Actividad8.java
+++ Actividad8punto1/src/actividad8punto1/Actividad8.java
@@ -4,50 +4,48 @@ public class Actividad8 {
 
     public static void main(String[] args) {
 
-            nodo Cabeza = new nodo();
-nodo recorrer = Cabeza;
-int contador = 80;	
- 
-while (contador <= 100) {
- 	recorrer.informacion.numero = String.valueOf (contador); 
-        if (contador < 100)  {
- 	recorrer.siguiente = new nodo();
-        recorrer = recorrer.siguiente;
-          }
- 	
-        contador += 2;	
-        }   	
-recorrer = Cabeza;	
-while (recorrer != null) {
- 	System.out.println(recorrer.informacion.numero);
+        nodo Cabeza = new nodo();
+        nodo recorrer = Cabeza;
+        int contador = 80;
+
+        while (contador <= 100) {
+            recorrer.informacion.numero = String.valueOf(contador);
+            if (contador < 100) {
+                recorrer.siguiente = new nodo();
                 recorrer = recorrer.siguiente;
-         }
-        
-        
+            }
+
+            contador += 2;
+        }
+        recorrer = Cabeza;
+        while (recorrer != null) {
+            System.out.println(recorrer.informacion.numero);
+            recorrer = recorrer.siguiente;
+        }
+
         nodo cabeza = null;
         int opcion = 0;
-        
-     
+
         while (opcion != 6) {
             System.out.println("1. agregar un nodo al final");
             System.out.println("2. agregar inicial un nodo");
-            
+
             System.out.println("3. mostrar lista");
             System.out.println("6. salir");
             System.out.print("digite el numero de la opcion deseada: ");
             opcion = Leer.datoInt();
-            
-               if (opcion == 1) {
+
+            if (opcion == 1) {
                 nodo nodo = new nodo();
                 System.out.println("ingrese un numero:");
                 nodo.informacion.numero = Leer.dato();
-                
-                Cabeza = new Operaciones().añadir(Cabeza, nodo);
+
+                Cabeza = new Operaciones().añadirAlFinal(Cabeza, nodo);
             } else if (opcion == 2) {
                 nodo nodo = new nodo();
                 System.out.println("ingrese numero para agregar al principio :");
                 nodo.informacion.numero = Leer.dato();
-                Cabeza = new Operaciones().añadir2(Cabeza, nodo);
+                Cabeza = new Operaciones().añadirAlInicio(Cabeza, nodo);
             } else if (opcion == 3) {
                 new Operaciones().listar(Cabeza);
             }
@@ -55,4 +53,4 @@ while (recorrer != null) {
         System.out.println("gracias por usarnos.");
     }
 
-    }
+}
